80443d7ac36409650460f942915f5f7e427c6731
bingzhao <dange@LENOVO-PC>
2017-02-06 13:57:38 +0800

    test change 2.

--- ConsoleApplication3/Day2.cs
+++ ConsoleApplication3/Day2.cs
@@ -16,11 +16,13 @@ namespace Day2
             Console.WriteLine("$ {0:0,0.00}", cash); // , inside the bracket denote thousand seperator. {0:0,0} is the same as {0:0,000}. Choose the later version for readability
             Console.WriteLine("$ {0:0,000.00}", cash);
             Console.WriteLine("$ {0}", cash);
+
             double x = 123; //double y = 33.334;
             Console.WriteLine("{0:#.#}",x);
             Console.WriteLine("{0:0}", x);    //123
             Console.WriteLine("{0:0000}", x); //0123
             Console.WriteLine("{0:##00}", x); //123, # means optional. If there is C# will print, otherwise, it will not
+            Console.WriteLine("{0:0.000}", x);
         }
     }
 }
